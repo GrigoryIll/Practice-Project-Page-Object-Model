092c7f82f44e9fafff614ba0813f6b45813434b9
GrigoryIll <stcky250@gmail.com>
2025-05-16 23:11:16 +0000

    third commit

--- tests/test_feature_profile.py
+++ tests/test_feature_profile.py
@@ -2,6 +2,7 @@ from base.test_base import TestBase
 import random
 import allure
 import pytest
+from selenium import webdriver
 
 
 @allure.feature("Profile Functionality")
@@ -13,9 +14,13 @@ class TestFeatureProfile(TestBase):
     def test_change_profile_name(self):
         self.login_page.open()
         self.login_page.enter_login(self.data.LOGIN)
+        print(self.data.LOGIN)
         self.login_page.enter_password(self.data.PASSWORD)
+        print(self.data.PASSWORD)
         self.login_page.submit_button()
-        # self.dashboard_page.is_opened()
+        driver = webdriver.Chrome()
+        print(driver.current_url)
+        self.dashboard_page.is_opened()
         self.dashboard_page.click_my_info_link()
         self.personal_page.change_name(f"Test{random.randint(1000, 10000)}")
         self.personal_page.save_changes()
